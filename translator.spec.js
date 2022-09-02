585dd4e492d943743edc70e620c60526f2225c3a
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-02 15:16:54 +0100

    tests for translateToEnglish written

--- translator.spec.js
+++ translator.spec.js
@@ -51,5 +51,57 @@ describe('Testing translateToMorse', () => {
     // Assert
     expect(translation).toBe(".- / -. . .-- / .-.. .. - - .-.. . / -.. --- --. .-.-.-");
   })
+ })
 
+ describe('Testing translateToEnglish', () => {
+  it('single symbol returns corresponding letter', ()=> {
+    // Arrange
+      let letter;
+      let translation;
+    // Act
+      letter = ".-";
+      translation= translateToMorse(letter);
+    // Assert
+    expect(translation).toBe("a");
+  })
+  it('symbols returns corresponding word', ()=> {
+    // Arrange
+      let word;
+      let translation;
+    // Act
+      word = "-.-. .- -";
+      translation= translateToMorse(word);
+    // Assert
+    expect(translation).toBe("cat");
+  })
+  it(' symbols separated by / returns corresponding sentence', ()=> {
+    // Arrange
+      let sentence;
+      let translation;
+    // Act
+      sentence = "- .... . / --.- ..- .. -.-. -.- / -... .-. --- .-- -. / ..-. --- -..- / .--- ..- -- .--. ... / --- ...- . .-. / - .... . / .-.. .- --.. -.-- / -.. --- --.";
+      translation= translateToMorse(sentence);
+    // Assert
+    expect(translation).toBe("the quick brown fox jumps over the lazy dog");
+  })   
+  it('invalid symbol returns error message', ()=> {
+    // Arrange
+      let letter;
+      let translation;
+    // Act
+      letter = "f/";
+      translation= translateToMorse(letter);
+    // Assert
+    expect(translation).toBe("Invalid input");
+  })
+  it('returns punctuated sentence fully translated', ()=> {
+    // Arrange
+    let translation;
+      let word;
+    // Act
+      word = ".- / -. . .-- / .-.. .. - - .-.. . / -.. --- --. .-.-.-";
+      translation= translateToMorse(word);
+    // Assert
+    expect(translation).toBe("a new little dog.");
+  })
  })
