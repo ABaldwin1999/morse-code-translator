72ee2bc708e82c9f6f0e54a0d18c53b9f6c9d372
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-02 10:12:12 +0100

    Started constructing translation function

--- translator.js
+++ translator.js
@@ -1,3 +1,13 @@
-export const translateToMorse =()=>{
+const latin =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+const morse=[".-","-...","-.-.",".--",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+export const translateToMorse =(message)=>{
+    let wordArray = message.split(' ');
+    wordArray.forEach((word) => {
+        let letterArray = word.split('');
+        letterArray.forEach((letter) =>{
+            letter.toUpperClass()
+        })
+    });
 
 }
