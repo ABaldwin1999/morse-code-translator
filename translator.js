de9738e768e3daa58ea1330cd5a4b7ee7c60cc93
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-02 11:06:52 +0100

    basic translate to morse function debugged

--- translator.js
+++ translator.js
@@ -1,17 +1,20 @@
 const latin =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const morse=[".-","-...","-.-.",".--",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+const morse=[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 
 export const translateToMorse =(message)=>{
     const wordArray = message.split(' ');
+    const sentenceArray =[];
     wordArray.forEach((word) => {
         const letterArray = word.split('');
         if(sentenceArray.length >0){
-            sentenceArray.push('/ ');
+            sentenceArray.push(' / ');
         }
         letterArray.forEach((letter) =>{
-            sentenceArray.push(morse[latin.indexOf(letter.toUpperClass())]);
-            sentenceArray.push(' ');
+            sentenceArray.push(morse[latin.indexOf(letter.toUpperCase())]);
+            if((letterArray.indexOf(letter) != letterArray.length -1)){
+                sentenceArray.push(' ');
+            }
         })
     });
-
+    return sentenceArray.join('');
 }
--- translator.spec.js
+++ translator.spec.js
@@ -4,6 +4,7 @@ describe('Testing translateToMorse', () => {
   it('single letter returns corresponding symbol', ()=> {
     // Arrange
       let letter;
+      let translation;
     // Act
       letter = "a";
       translation= translateToMorse(letter);
@@ -13,6 +14,7 @@ describe('Testing translateToMorse', () => {
   it('word returns corresponding symbols', ()=> {
     // Arrange
       let word;
+      let translation;
     // Act
       word = "cat";
       translation= translateToMorse(word);
@@ -22,6 +24,7 @@ describe('Testing translateToMorse', () => {
   it('sentence returns corresponding symbols', ()=> {
     // Arrange
       let sentence;
+      let translation;
     // Act
       sentence = "the quick brown fox jumps over the lazy dog";
       translation= translateToMorse(sentence);
@@ -31,15 +34,17 @@ describe('Testing translateToMorse', () => {
   it('returns symbol for both upper and lower cases', ()=> {
     // Arrange
       let letter;
+      let translation;
     // Act
-      word = "A";
+      letter = "A";
       translation= translateToMorse(letter);
     // Assert
     expect(translation).toBe(".-");
   })
   it('returns punctuated sentence fully translated', ()=> {
     // Arrange
-      let letter;
+    let translation;
+      let word;
     // Act
       word = "a new little dog.";
       translation= translateToMorse(letter);
